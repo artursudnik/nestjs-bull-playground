9999f3d72daf7b0dde69eff27874d5e68c7aebab
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2023-01-03 23:07:04 +0100

    feat: adding event listeners

--- src/audio.processor.ts
+++ src/audio.processor.ts
@@ -1,4 +1,14 @@
-import { Process, Processor } from '@nestjs/bull';
+import {
+  OnQueueActive,
+  OnQueueCleaned,
+  OnQueueCompleted,
+  OnQueueDrained,
+  OnQueueFailed,
+  OnQueueRemoved,
+  OnQueueStalled,
+  Process,
+  Processor,
+} from '@nestjs/bull';
 import { Job } from 'bull';
 import { Logger } from '@nestjs/common';
 import { setTimeout } from 'timers/promises';
@@ -17,5 +27,42 @@ export class AudioProcessor {
   @Process(TRANSCODING_JOB_TYPE_NAME)
   async transcode(job: Job<{ foo: string }>) {
     await setTimeout(1000);
+  }
+
+  @OnQueueActive()
+  onActive(job: Job) {
+    this.logger.debug(
+      `Processing job ${job.id} with data ${JSON.stringify(job.data)}`,
+    );
+  }
+
+  @OnQueueCompleted()
+  onCompleted(job: Job) {
+    this.logger.debug(`job ${job.id} completed`);
+  }
+
+  @OnQueueStalled()
+  onStalled(job: Job) {
+    this.logger.debug(`job ${job.id} stalled`);
+  }
+
+  @OnQueueFailed()
+  onFailed(job: Job) {
+    this.logger.debug(`job ${job.id} failed`);
+  }
+
+  @OnQueueRemoved()
+  onRemoved(job: Job) {
+    this.logger.debug(`job ${job.id} removed`);
+  }
+
+  @OnQueueDrained()
+  onDrained() {
+    this.logger.debug(`queue drained`);
+  }
+
+  @OnQueueCleaned()
+  onCleaned() {
+    this.logger.debug(`queue cleaned`);
   }
 }
