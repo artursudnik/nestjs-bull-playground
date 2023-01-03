bd6d2bf6a397323baf1a7147f30576402621abcc
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2023-01-03 21:34:25 +0100

    feat: implementing addJob service method

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bull';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name, { timestamp: true });
+
+  constructor(@InjectQueue('audio') private audioQueue: Queue) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async addJob(): Promise<void> {
+    this.logger.debug('adding job');
+
+    const job = await this.audioQueue.add('transcoding', {
+      foo: 'bar',
+    });
+
+    this.logger.debug(`job ${job.id} added`);
   }
 }
