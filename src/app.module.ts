fc841f4d2c9027fd49d4a7984b3a2d5ba8569aa7
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2023-01-03 21:31:19 +0100

    wip: importing the `BullModule` and `audio` queue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: '127.0.0.1',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'audio',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
